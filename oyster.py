99faa6834890c6d04c69137f645f66e0e38af731
Francisco Dans <fdansv@gmail.com>
2013-05-03 21:12:12 +0100

    added imports, get

--- oyster.py
+++ oyster.py
@@ -1,9 +1,15 @@
-import urllib, urllib2
-from BeautifulSoup import BeautifulSoup as BS
-
-data = urllib.urlencode({'j_username':'FDANSV','j_password':'Fdv090190'})
-u = urllib2.urlopen('https://oyster.tfl.gov.uk/oyster/security_check',data)
+import urllib
+import urllib2
+import cookielib
+from bs4 import BeautifulSoup as BS
+cookies = cookielib.CookieJar()
+data = urllib.urlencode({'j_username': '', 'j_password': ''})
+u = urllib2.urlopen('https://oyster.tfl.gov.uk/oyster/security_check', data)
+req = urllib2.Request(u)
 soup = BS(u.read())
+cards = []
 for op in soup.form.findAll('option'):
     if op['value']:
-        print op['value']
+        cards.append(op['value'])
+print cards
+cookies.extract_cookies(um,req)
