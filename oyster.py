59da8c6c5ebb2d510c9698c4b2916ff82b8b7b0e
Francisco Dans <fdansv@gmail.com>
2013-05-06 02:50:47 +0100

    First loop through journey history

--- oyster.py
+++ oyster.py
@@ -1,15 +1,30 @@
 import urllib
 import urllib2
 import cookielib
-from bs4 import BeautifulSoup as BS
+from BeautifulSoup import BeautifulSoup as BS
+print 'Oyster chekah 1 loading...'
 cookies = cookielib.CookieJar()
-data = urllib.urlencode({'j_username': '', 'j_password': ''})
-u = urllib2.urlopen('https://oyster.tfl.gov.uk/oyster/security_check', data)
-req = urllib2.Request(u)
-soup = BS(u.read())
+data = urllib.urlencode({'': '', '': ''})
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
+res = opener.open('https://oyster.tfl.gov.uk/oyster/security_check', data)
+soup = BS(res.read())
 cards = []
 for op in soup.form.findAll('option'):
     if op['value']:
         cards.append(op['value'])
-print cards
-cookies.extract_cookies(um,req)
+datacard = urllib.urlencode({'method':'input', 'cardId':'056347247367'})
+res = opener.open('https://oyster.tfl.gov.uk/oyster/selectCard.do', datacard)
+journeyhistorydata = urllib.urlencode({'dateRange':'01/03/2013 00:00:00-04/05/2013 23:59:59','offset':'0','rows':'0','customDateRangeSel':'false','isJSEnabledForPagenation':'true'})
+res = opener.open('https://oyster.tfl.gov.uk/oyster/journeyHistory.do', journeyhistorydata)
+history = BS(res.read()).find('table', 'journeyhistory')
+for roudata in history.findAll('tr'):
+	if str(roudata.find('status-1')!=-1):
+		try:
+			print roudata.findAll('td')[1].string
+		except:
+			pass
+	try:
+		if roudata.findAll('td')[1].string == None:
+			print roudata.findAll('td')[0].string
+	except:
+		pass
